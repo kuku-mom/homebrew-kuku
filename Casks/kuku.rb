9c75f807c4c6f1165dfcb29e7271aa44e11f1f6a
GyeongTaek Kim <mansuiki@gmail.com>
2026-02-04 14:41:16 +0900

    0.2.5

--- Casks/kuku.rb
+++ Casks/kuku.rb
@@ -1,5 +1,5 @@
 cask "kuku" do
-  version "0.2.4"
+  version "0.2.5"
   sha256 "584ec6825b26a27437f6ea41fd1e3ca7321510477c7781e74fd300e97b2f9998"
 
   url "https://github.com/kuku-mom/kuku/releases/download/#{version}/Kuku.app.tar.gz"
