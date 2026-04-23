f1b5903d3b0b5ddd1b447ceaa3cc166db1186c99
GyeongTaek Kim <mansuiki@gmail.com>
2026-04-23 13:41:13 +0900

    0.3.1

--- Casks/kuku.rb
+++ Casks/kuku.rb
@@ -1,8 +1,8 @@
 cask "kuku" do
-  version "0.3.0"
-  sha256 "ba29e6a25f3b8de9cdfde0c80f2a158052dad8892424aae1cc6c2bb85ecb399f"
+  version "0.3.1"
+  sha256 "09b3d4ba44dd5ead0cd32148deebe0c4276e98951f96c7df3bd51f75235d85ae"
 
-  url "https://github.com/kuku-mom/kuku/releases/download/0.3.0/Kuku.app.tar.gz"
+  url "https://github.com/kuku-mom/kuku/releases/download/0.3.1/Kuku.app.tar.gz"
   name "Kuku"
   desc "Like Obsidian, but it learned some new tricks"
   homepage "https://www.kuku.mom"
